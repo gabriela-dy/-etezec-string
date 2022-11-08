acb5fcdabc247e82679f2b473e9944f8fdeed64b
Gabriela Dyworová <gabriela.dyworova@gmail.com>
2022-11-08 13:00:42 +0100

    Přidejte soubory projektu.

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿string s = "Krokonosohroch";
+Console.WriteLine(s.StartsWith("krok"));
+Console.WriteLine(s.EndsWith("hroch"));
+Console.WriteLine(s.Contains("nos"));
+Console.WriteLine(s.Contains("roh"));
+Console.ReadKey();
